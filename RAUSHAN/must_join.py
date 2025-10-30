731e0bd73b819cf7d668462f00c771c0ccdd343b
𓆩⭕️ᴋ ʀ ɪ s ʜ🔻ᴋ ᴀ ᴩ ᴜ ʀ⭕️𓆪 <rarebit100@gmail.com>
2025-10-31 01:13:37 +0530

    Update must_join.py

--- RAUSHAN/must_join.py
+++ RAUSHAN/must_join.py
@@ -14,7 +14,7 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/llDPZ_EDITIXll" + MUST_JOIN
+                link = "https://t.me/rarebit_gamers" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
@@ -24,7 +24,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("🔶 ᴊᴏɪɴ ᴏғғɪᴄᴇ 🔶", url = "https://t.me/llDPZ_EDITIXll"),
+                                InlineKeyboardButton("🔶 ᴊᴏɪɴ ᴏғғɪᴄᴇ 🔶", url = "https://t.me/rarebit_gamers"),
                             ]
                         ]
                     )
